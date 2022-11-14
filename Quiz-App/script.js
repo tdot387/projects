a7f6bb948fd9c781261ca2b7e548406249f61673
Thomas Wagner <t-wagner@web.de>
2022-11-14 20:14:16 +0100

    day's work accomplished

---
+++ Quiz-App/script.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+        "question": "Mit welchem Tier identifiziert sich Frenky am meisten?",
+        "answer1": "Waldsprecht",
+        "answer2": "Saufkuh",
+        "answer3": "Fettaal",
+        "answer4": "Giraffe",
+        "right_answer": 2
+    },
+    {
+        "question": "Mit welchem Tier identifiziert sich Frenky am meisten?",
+        "answer1": "Waldsprecht",
+        "answer2": "Saufkuh",
+        "answer3": "Fettaal",
+        "answer4": "Giraffe",
+        "right_answer": 2
+    },
+    {
+        "question": "Mit welchem Tier identifiziert sich Frenky am meisten?",
+        "answer1": "Waldsprecht",
+        "answer2": "Saufkuh",
+        "answer3": "Fettaal",
+        "answer4": "Giraffe",
+        "right_answer": 2
+    },
+    {
+        "question": "Mit welchem Tier identifiziert sich Frenky am meisten?",
+        "answer1": "Waldsprecht",
+        "answer2": "Saufkuh",
+        "answer3": "Fettaal",
+        "answer4": "Giraffe",
+        "right_answer": 2
+    },
+    {
+        "question": "Mit welchem Tier identifiziert sich Frenky am meisten?",
+        "answer1": "Waldsprecht",
+        "answer2": "Saufkuh",
+        "answer3": "Fettaal",
+        "answer4": "Giraffe",
+        "right_answer": 2
+    },
+    {
+        "question": "Mit welchem Tier identifiziert sich Frenky am meisten?",
+        "answer1": "Waldsprecht",
+        "answer2": "Saufkuh",
+        "answer3": "Fettaal",
+        "answer4": "Giraffe",
+        "right_answer": 2
+    },
+    {
+        "question": "Mit welchem Tier identifiziert sich Frenky am meisten?",
+        "answer1": "Waldsprecht",
+        "answer2": "Saufkuh",
+        "answer3": "Fettaal",
+        "answer4": "Giraffe",
+        "right_answer": 2
+    }
+
+];
