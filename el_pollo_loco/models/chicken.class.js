73c76ccb3aa2dee74c35edbf0403c5d58ec9e316
Thomas Wagner <t-wagner@web.de>
2022-12-08 20:37:44 +0100

    first commit

---
+++ el_pollo_loco/models/chicken.class.js
@@ -0,0 +1,8 @@
+class Chicken extends MoveableObject {
+
+
+    constructor() {
+        super().loadImage('./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+    }
+
+}
---
+++ el_pollo_loco/models/moveable-object.class.js
@@ -0,0 +1,20 @@
+class MoveableObject {
+    x = 120;
+    y = 400;
+    img;
+
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+    moveRight() {
+        console.log('Moving right');
+    }
+
+
+    moveLeft() {
+
+    }
+}
