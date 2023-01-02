d8bcfbbfaacd1ea5b575834e77ac5417dd68749b
Thomas Wagner <t-wagner@web.de>
2023-01-02 12:41:39 +0100

    updated shit

--- el_pollo_loco/models/character.class.js
+++ el_pollo_loco/models/character.class.js
@@ -13,6 +13,7 @@ class Character extends MoveableObject {
         './img/2_character_pepe/2_walk/W-26.png'
     ];
     world;
+    walking_sound = new Audio('audio/walking-sound.mp3');
 
     constructor() {
         super().loadImage('./img/2_character_pepe/2_walk/W-21.png');
@@ -25,15 +26,22 @@ class Character extends MoveableObject {
     animate() {
 
         setInterval(() => {
-            if (this.world.keyboard.RIGHT) {
+            this.walking_sound.pause();
+            
+            if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
+                this.walking_sound.play();
+
             }
 
-            if (this.world.keyboard.LEFT) {
+            if (this.world.keyboard.LEFT && this.x > 0) {
                 this.x -= this.speed;
                 this.otherDirection = true;
+                this.walking_sound.play();
+
             }
+            this.world.camera_x = -this.x + 100;
         }, 1000 / 60);
 
         
--- el_pollo_loco/models/world.class.js
+++ el_pollo_loco/models/world.class.js
@@ -1,24 +1,10 @@
 class World {
     character = new Character();
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken()
-    ];
-    clouds = [
-        new Cloud()
-    ];
-    backgroundObjects = [
-        new BackgroundObject("./img/5_background/layers/air.png", 0),
-        new BackgroundObject("./img/5_background/layers/3_third_layer/1.png", 0),
-        new BackgroundObject("./img/5_background/layers/2_second_layer/1.png", 0),
-        new BackgroundObject("./img/5_background/layers/1_first_layer/1.png", 0),
-
-    ]
-
+    level = level1;
     canvas;
     ctx;
     keyboard;
+    camera_x = 0;
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -35,12 +21,17 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        this.ctx.translate(this.camera_x, 0);
+
         this.addToMap(this.character);
 
-        this.addObjectsToMap(this.backgroundObjects);
+        this.addObjectsToMap(this.level.backgroundObjects);
         this.addToMap(this.character);
-        this.addObjectsToMap(this.clouds);
-        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.level.clouds);
+        this.addObjectsToMap(this.level.enemies);
+
+        this.ctx.translate(-this.camera_x, 0);
+
 
         // Draw() wird immer wieder aufgerufen. "this" erkennt er innerhalb der Fkt nicht, deswegen wurde die Variable "self" dafür erstellt.
         let self = this;
@@ -58,12 +49,15 @@ class World {
     addToMap(mo) {  //mo steht für moveable object
         if(mo.otherDirection) {
             this.ctx.save();
-            this.ctx.translate(mo.img.width, 0);
-            this.ctx.scale(-1,1);
+            this.ctx.translate(mo.width, 0);
+            this.ctx.scale(-1, 1);
+            mo.x = mo.x * -1;
         }
         this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
         if(mo.otherDirection) {
+            mo.x = mo.x * -1;
             this.ctx.restore();
+            
         }
     }
 }
